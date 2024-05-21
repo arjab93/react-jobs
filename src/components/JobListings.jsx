4d6d699c27408203a0049c08d249d01c3ee37804
Arjab Khadka <arjabkhadka93@gmail.com>
2024-05-21 19:10:09 +0545

    server created and loading effect added

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import jobs from '../jobs.json'
 import JobListing from './JobListing'
+import { useEffect, useState } from 'react';
+import Spinner from './Spinner';
 
-const JobListings = ({isHome = false}) => {
-    const jobListings = isHome ? jobs.slice(0,3) : jobs;
+const JobListings = ({ isHome = false }) => {
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            const apiUrl = isHome ?
+                'http://localhost:5000/jobs?_limit=3'
+                : 'http://localhost:5000/jobs'
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                setJobs(data);
+            } catch (error) {
+                console.log('Error fetching data', error);
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchJobs();
+    }, []);
 
     return (
         <>
@@ -12,14 +32,18 @@ const JobListings = ({isHome = false}) => {
                     <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                         {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                     </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {jobListings.map((job) => (
-                            <JobListing key={job.id} job={job} />
-                        ))}
 
-                    </div>
+                    {loading ? (
+                        <Spinner loading={loading} />
+                    ) : (
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            {jobs.map((job) => (
+                                <JobListing key={job.id} job={job} />
+                            ))}
+                        </div>
+                    )}
                 </div>
-            </section>
+            </section >
         </>
     )
 }
